f402fbccf5fecd001679f4576ee02d634444b591
Juliana Bernardes Pereira <julianabp96@gmail.com>
2023-04-06 11:05:09 +0100

    add side bar menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,80 @@
 <template>
-  <router-view/>
+  <Menu :class="[{'collapsed' : collapsed}]"/>
+  <div id="container" :class="[{'collapsed' : collapsed}]"> 
+    <router-view />
+  </div>
+  <sidebar-menu 
+    :menu="menu"
+    v-model:collapsed="collapsed"
+  />
 </template>
-
 <script>
 import Menu from './components/Menu.vue'
+import { SidebarMenu } from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import './styles/sidebar_menu.css'
+
 export default {
   name: 'App',
   components: {
-    Menu
+    Menu,
+    SidebarMenu
   },
   mounted() {
 
+  },
+  data() {
+    return {
+      menu: [
+        {
+          header: 'Matérias',
+          hiddenOnCollapse: true
+        },
+        {
+          href: '/',
+          title: 'Introdução',
+          icon: 'fa fa-user'
+        },
+        {
+          href: '/physics',
+          title: 'Física',
+          icon: 'fa fa-chart-area',
+          child: [
+            {
+              // href: '/physics/oblique_movement',
+              title: 'Movimento oblíquo',
+              child: [
+                {
+                  href: '/physics/oblique_movement/quizz',
+                  title: 'Quizz'
+                },
+                {
+                  href: '/physics/oblique_movement/experiment',
+                  title: 'Experimento'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      collapsed: false
+      
+    }
   },
 }
 </script>
 
 <style>
-#app {
-
+#container {
+  padding-left: 290px;
+  -webkit-transition: .3s ease;
+  transition: .3s ease
 }
+
+#container.collapsed,#container.onmobile {
+  padding-left: 65px;
+}
+
 </style>
+
+<style lang="css" src="./styles/sidebar_menu.css"></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import './css/style.css'
+import './styles/style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 
 
 createApp(App)
     .use(router)
+    .use(VueSidebarMenu)
     .mount('#app')
